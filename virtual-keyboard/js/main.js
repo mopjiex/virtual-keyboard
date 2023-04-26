0822e9cbccc3255649e30e99501d7118d0f34ed7
mopjiex <artem1bannikov@gmail.com>
2023-04-26 16:24:56 +0300

    refactor: small rework of the keyboard output implementation

--- virtual-keyboard/js/main.js
+++ virtual-keyboard/js/main.js
@@ -1,4 +1,3 @@
-
 const createElements = () => {
     const body = document.querySelector('body');
     const header = document.createElement('header');
@@ -9,117 +8,72 @@ const createElements = () => {
     const headerTitle = document.createElement('h1');
     headerTitle.className = 'header__title';
     headerTitle.textContent = 'Виртуальная клавиатура';
-    header.append(container, headerTitle);
-
+    header.append(container);
+    const headerText = document.createElement('p');
+    headerText.className = 'header__text';
+    headerText.textContent = 'Создана в Windows';
+    container.append(headerTitle, headerText);
     const main = document.createElement('main');
     main.className = 'main';
     body.append(main);
     const keyboard = document.createElement('section');
     keyboard.className = 'keyboard';
     main.append(keyboard);
-    keyboard.append(container);
+    const container2 = document.createElement('div');
+    container2.className = 'container';
+    keyboard.append(container2);
     const keyboardInner = document.createElement('div');
     keyboardInner.className = 'keyboard__inner';
-    container.append(keyboardInner)
+    container2.append(keyboardInner)
     const keyboardTextarea = document.createElement('textarea');
     keyboardTextarea.className = 'keyboard__textarea';
     keyboardInner.append(keyboardTextarea);
-    
     const keyboardKeys = document.createElement('div');
     keyboardKeys.className = 'keyboard__keys';
     keyboardInner.append(keyboardKeys);
 
-    const keyboardKeysRow1 = document.createElement('div');
-    keyboardKeysRow1.className = 'keyboard__keys-row';
-    const keysRow1 = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
-    keysRow1.forEach((item, index) => {
+    const keys = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', //13
+                  'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', //26
+                  'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', //39
+                  'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', 'Shift', //49
+                  'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Ctrl',];
+
+    keys.forEach((item, index) => {
         const key = document.createElement('div');
-        key.className = 'keyboard__key ' + ((index <= 1) ? "red" : (index <= 3) ? "orange" : (index <= 5) ? "yellow" : (index <= 10) ? "green" : "blue");
-        if(index == keysRow1.length - 1) {
-            key.className = 'keyboard__key backspace blue';
-        }
         key.textContent = item;
-        keyboardKeysRow1.append(key);
-    });
+        if ([0, 1].includes(index)) {
+            key.className = 'keyboard__key red';
+        }
+        if ([2, 3, 15, 16, 28].includes(index)) {
+            key.className = 'keyboard__key orange';
+        }
 
-    keyboardKeys.append(keyboardKeysRow1);
+        if ([4, 5, 17, 18, 29, 30, 40, 41].includes(index)) {
+            key.className = 'keyboard__key yellow';
+        }
 
-    const keyboardKeysRow2 = document.createElement('div');
-    keyboardKeysRow2.className = 'keyboard__keys-row';
-    const keysRow2 = ["Tab", "й", "ц", "у", "к", "у", "н", "г", "ш", "щ", "з", "х", "ъ", "\\", 'Del'];
-    keysRow2.forEach((item, index) => {
-       const key = document.createElement('div');
-       key.className = "keyboard__key " + ((index === 0) ? "red tab" : (index <= 2) ? "orange" : (index <= 4) ? "yellow" : (index <= 10) ? "green" : "blue del");
-       key.textContent = item;
-       keyboardKeysRow2.append(key);
-    });
+        if ([6, 7, 8, 9, 10, 19, 20, 21,22, 23, 24, 31, 32, 33, 34, 35, 36, 42, 43, 44, 45, 46, 47].includes(index)) {
+            key.className = 'keyboard__key green';
+        }
 
-    keyboardKeys.append(keyboardKeysRow2);
+        if ([12, 25, 26, 37, 38, 48, 49, 50].includes(index)) {
+            key.className = 'keyboard__key blue';
+        }
 
-    const keyboardKeysRow3 = document.createElement('div');
-    keyboardKeysRow3.className = 'keyboard__keys-row';
-    const keysRow3 = ["CapsLock", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "Enter"];
-    keysRow3.forEach((item, index) => {
-       const key = document.createElement('div');
-       key.className = "keyboard__key " + ((index === 0) ? "orange caps" : (index <= 2) ? "yellow" : (index <= 8) ? "green" : "blue");
-       if(index == keysRow3.length - 1) {
-        key.className = 'keyboard__key enter blue';
-       }
-       key.textContent = item;
-       keyboardKeysRow3.append(key);
-    });
-
-    keyboardKeys.append(keyboardKeysRow3);
-
-    const keyboardKeysRow4 = document.createElement('div');
-    keyboardKeysRow4.className = 'keyboard__keys-row';
-    const keysRow4 = ["Shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".", "Shift"];
-    keysRow4.forEach((item, index) => {
-       const key = document.createElement('div');
-       key.className = "keyboard__key " + (index < 2 ? "orange" : index < 4 ? "yellow" : "green");
-       if(index == 0) {
-        key.className = 'keyboard__key enter orange'
-       }
-       if(index == keysRow4.length - 1) {
-        key.className = 'keyboard__key shift blue';
-       }
-       key.textContent = item;
-       keyboardKeysRow4.append(key);
-    });
-
-    keyboardKeys.append(keyboardKeysRow4);
-
-    const keyboardKeysRow5 = document.createElement('div');
-    keyboardKeysRow5.className = 'keyboard__keys-row';
-    const keysRow5 = ['Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl'];
-    keysRow5.forEach((item, index) => {
-       const key = document.createElement('div');
-       
-       if(index == 0) {
-        key.className = 'keyboard__key ctrl orange';
-       }
-       if(index == 1) {
-        key.className = 'keyboard__key win yellow';
-       }
-       if(index == 2) {
-        key.className = 'keyboard__key alt yellow';
-       }
-       if(index == 3) {
-        key.className = 'keyboard__key space';
-       }
-       if(index == 4) {
-        key.className = 'keyboard__key alt blue';
-       }
-       if(index == keysRow5.length - 1) {
-        key.className = 'keyboard__key ctrl blue';
-       }
-
-       key.textContent = item;
-       keyboardKeysRow5.append(key);
-    });
-
-    keyboardKeys.append(keyboardKeysRow5);
-
+         if(index == 13) key.className = 'keyboard__key blue backspace';
+         if(index == 14) key.className = 'keyboard__key red tab';
+         if(index == 27) key.className = 'keyboard__key orange caps';
+         if(index == 39) key.className = 'keyboard__key blue enter';
+         if(index == 40) key.className = 'keyboard__key orange shift';
+         if(index == 50) key.className = 'keyboard__key blue shift';
+         if(index == 51) key.className = 'keyboard__key orange ctrl';
+         if(index == 56) key.className = 'keyboard__key blue ctrl';
+         if(index == 52) key.className = 'keyboard__key yellow win';
+         if(index == 53) key.className = 'keyboard__key yellow alt';
+         if(index == 55) key.className = 'keyboard__key blue alt';
+         if(index == 54) key.className = 'keyboard__key space';
+        keyboardKeys.append(key);
+    })
 }
-
 createElements();
+
